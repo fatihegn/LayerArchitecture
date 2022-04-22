8201a4f99ad39299cf0930ee31c5d88ad6df8abe
50ot <332061036@ogr.uludag.edu.tr>
2022-04-22 03:59:31 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Max Fiyata Göre Listele : ");
+            int unitValue = Convert.ToInt32(Console.ReadLine());
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetByUnitPrice(unitValue))
+            {
+                Console.WriteLine("Max "+unitValue+" değerindeki ürünler.");
+                Console.Write(item.ProductName);
+                Console.WriteLine(" Fiyatı : "+item.UnitPrice);
+            }
+          
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using System.Linq.Expressions;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            // Hafızadaki hazır DB olarak kabul ediyoruz.
+            _products = new List<Product> {
+            new Product{ProductId=1,CategoryId=1,ProductName="Tea",UnitPrice=5,UnitsInStock=15},
+            new Product{ProductId=2,CategoryId=1,ProductName="Coffe",UnitPrice=10,UnitsInStock=15},
+            new Product{ProductId=3,CategoryId=1,ProductName="Water",UnitPrice=2,UnitsInStock=15},
+            new Product{ProductId=4,CategoryId=2,ProductName="Muffin",UnitPrice=15,UnitsInStock=15},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete =
+            _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p=>p.CategoryId==categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderilen ürün id'sine sahip ürünü listedeki ürünü bulmak . / Linq
+            Product productToUpdate =
+             _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+        }
+    }
+}
